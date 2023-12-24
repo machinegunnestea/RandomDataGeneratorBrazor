485c25f7e3f4f43c0d7a0af8176b266b00c1f413
m0nk3y m4n <igreading@mail.ru>
2023-12-25 02:41:23 +0300

    cleanup code

--- RandomDataBrazor/Program.cs
+++ RandomDataBrazor/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddTransient<DataGenerator>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices(); 
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
